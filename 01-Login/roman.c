4e1ef560cbe56ecb19f096930ce65e8183354afe
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-01-24 15:59:38 +0530

    Create roman.c

---
+++ 01-Login/roman.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+scanf("%s",&ch);
+switch(ch)
+{
+case 'I':
+printf("1");
+break;
+case 'II':
+printf("2");
+break;
+case 'III':
+printf("3");
+break;
+case 'IV':
+printf("4");
+break;
+case 'v':
+printf("5");
+break;
+case 'VI':
+printf("6");
+break;
+case 'VII':
+printf("7");
+break;
+case '':
+printf("1");
+break;
+case 'I':
+printf("1");
+break;
+case 'I':
+printf("1");
+break;
